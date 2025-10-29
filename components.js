8f46fea08b68c75fab13b16bc2baf6f266211233
hattimatimtimnmf <hattimatimtim.creators@gmail.com>
2025-10-30 01:33:41 +0530

    Updated js

--- components.js
+++ components.js
@@ -1,39 +1,59 @@
 // --- REUSABLE HTML COMPONENTS ---
 
-// 1. Navigation Bar HTML (Header) - ALL HREFs ARE NOW ROOT-RELATIVE
+// Function to generate the correct path based on the current page location
+function getPath(filename) {
+    // Check if the current page URL path includes the '/test/' subdirectory
+    const isInsideTestFolder = window.location.pathname.includes('/test/');
+
+    if (isInsideTestFolder) {
+        // If inside the /test/ folder, go up one level (../) to find the main files.
+        // We link to anchors on the home page via index.html
+        if (filename.startsWith('#')) {
+            return 'index.html' + filename;
+        }
+        // General link to parent folder files
+        return '../' + filename;
+    } else {
+        // If in the root folder, link directly to the file in the current directory.
+        return filename;
+    }
+}
+
+// 1. Navigation Bar HTML (Header) - Uses getPath()
 const headerHTML = `
     <nav class="navbar">
         <div class="nav-brand">
-            <a href="website/index.html" class="nav-item">Hattimatimtim 🏡</a>
+            <a href="${getPath('index.html')}" class="nav-item">Hattimatimtim 🏡</a>
         </div>
         <div class="nav-left" id="nav-links">
-            <a href="../lessons.html" class="nav-item">Lessons</a>
-            <a href="../mock_test.html" class="nav-item">Mock Test</a>
-            <a href="../#games" class="nav-item">Play Zone</a>
-            <a href="../#stories" class="nav-item">Story Time</a>
-            <a href="../#about" class="nav-item">About Us</a>
+            <a href="${getPath('lessons.html')}" class="nav-item">Lessons</a>
+            <a href="${getPath('mock_test.html')}" class="nav-item">Mock Test</a>
+            <a href="${getPath('#games')}" class="nav-item">Play Zone</a>
+            <a href="${getPath('#stories')}" class="nav-item">Story Time</a>
+            <a href="${getPath('#about')}" class="nav-item">About Us</a>
         </div>
         <div class="nav-right">
-            <a href="/#account" class="nav-item account-btn">My Account</a>
+            <a href="${getPath('#account')}" class="nav-item account-btn">My Account</a>
         </div>
+        <!-- Hamburger menu icon for mobile -->
         <button class="menu-toggle" aria-label="Toggle Navigation">☰</button>
     </nav>
 `;
 
-// 2. Footer HTML - ALL HREFS ARE NOW ROOT-RELATIVE
+// 2. Footer HTML - Uses getPath()
 const footerHTML = `
     <footer class="main-footer">
         <div class="footer-links">
-            <a href="../index.html#about">About Us</a> |
-            <a href="../index.html#contribute">Contribute</a> |
-            <a href="../index.html#feedback">Feedback</a> |
-            <a href="../index.html#privacy">Privacy</a>
+            <a href="${getPath('index.html#about')}">About Us</a> |
+            <a href="${getPath('index.html#contribute')}">Contribute</a> |
+            <a href="${getPath('index.html#feedback')}">Feedback</a> |
+            <a href="${getPath('index.html#privacy')}">Privacy</a>
         </div>
         <p>© 2025 Hattimatimtim. All Rights Reserved. Made with fun and imagination!</p>
     </footer>
 `;
 
-// --- INJECTION FUNCTION ---
+// --- INJECTION FUNCTION (rest remains the same) ---
 
 function loadGlobalComponents() {
     // Inject Header
